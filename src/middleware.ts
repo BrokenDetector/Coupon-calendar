f0b792f6c56f851e0c2f1980e4504ccc39117cdb
BrokenDetector <99690426+BrokenDetector@users.noreply.github.com>
2025-08-16 18:43:39 +0300

    fix(middleware): resolve OPTIONS 400 errors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,35 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
+const allowedOrigins = [
+	"https://coupon-calendar.vercel.app",
+	"https://coupon-calendar-git-dev-detectors-projects.vercel.app",
+	"http://localhost:3000",
+];
+
+const corsOptions = {
+	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+	"Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export async function middleware(req: NextRequest) {
 	const session = await getToken({ req });
 	const pathname = req.nextUrl.pathname;
+
+	// Check the origin from the request
+	const origin = req.headers.get("origin") ?? "";
+	const isAllowedOrigin = allowedOrigins.includes(origin);
+
+	// Handle preflighted requests
+	const isPreflight = req.method === "OPTIONS";
+
+	if (isPreflight) {
+		const preflightHeaders = {
+			...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+			...corsOptions,
+		};
+		return NextResponse.json({}, { headers: preflightHeaders });
+	}
 
 	if (!session && pathname.startsWith("/portfolio")) {
 		return NextResponse.redirect(new URL("/auth", req.url));
