3054176a864903b11f2aa4e8968d32c6993721a8
BrokenDetector <99690426+BrokenDetector@users.noreply.github.com>
2025-09-17 18:14:14 +0300

    chore: migrate eslint to flag config and setup prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+});
+
+export default defineConfig([
+	{
+		extends: compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+
+		rules: {
+			"@typescript-eslint/no-explicit-any": "warn",
+			"no-unused-vars": "off",
+
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					argsIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					caughtErrorsIgnorePattern: "^_",
+				},
+			],
+		},
+	},
+]);
